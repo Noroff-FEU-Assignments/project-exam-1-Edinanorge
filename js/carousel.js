17731fd4dbdc835c7717eecdf2ca9f91acf86920
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2022-11-07 13:03:19 +0100

    responsive test

--- js/carousel.js
+++ js/carousel.js
@@ -5,15 +5,10 @@ carousels.forEach((carousel) => {
   const dotsHtml = Array.from(carouselItems, () => {
     return `<button class="dot">&nbsp;</button>`;
   });
-  const buttonsHtml = `<button class="btn-arrow btn-left "><i class="fa-solid fa-chevron-left"></i></button>
-                        <button class="btn-arrow btn-right btn--selected "><i class="fa-solid fa-chevron-right"></i></button>`;
 
-  carousel.insertAdjacentHTML("beforeend", buttonsHtml);
   carousel.insertAdjacentHTML("beforeend", `<div class="dots">${dotsHtml.join("")}</div>`);
 
   const dots = carousel.querySelectorAll(".dot");
-  const btnLeft = carousel.querySelector(".btn-left");
-  const btnRight = carousel.querySelector(".btn-right");
 
   dots.forEach((dot, i) => {
     dot.addEventListener("click", () => {
