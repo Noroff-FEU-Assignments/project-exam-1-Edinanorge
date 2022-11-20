efd9295b2e4eb3429451123c1eed95d86bee8580
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2022-11-20 21:38:05 +0100

    sticky nav

--- js/components/navigation.js
+++ js/components/navigation.js
@@ -1,6 +1,18 @@
 const btnNav = document.querySelector(".btn-mobile-nav");
 const navContainer = document.querySelector(".navigation-container");
 
-btnNav.addEventListener("click", function () {
-  navContainer.classList.toggle("nav-open");
+btnNav.addEventListener("click", () => navContainer.classList.toggle("nav-open"));
+
+window.addEventListener("scroll", () => {
+  const scrollable = window.innerHeight;
+
+  const scrolled = window.scrollY;
+
+  console.log(navContainer.classList);
+  if (scrollable <= Math.ceil(scrolled)) {
+    document.body.classList.add("sticky");
+    console.log(navContainer.classList);
+  } else if (scrollable > Math.ceil(scrolled)) {
+    document.body.classList.remove("sticky");
+  }
 });
